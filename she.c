880969c447e6360009b9a3d87a56ba0a42cfa9d3
Tanish Verma <astronomicaltanish@gmail.com>
2023-09-07 18:06:14 +0530

    ye

--- she.c
+++ she.c
@@ -8,29 +8,33 @@
 char* read_user_input() {
     char* buffer = (char*)malloc(MAX * sizeof(char));
     // Read user input dynamically
-    fgets(buffer, MAX, stdin);
-    int length=sizeof(buffer);
+    //fgets(buffer, MAX, stdin);
 
     if (buffer==NULL){             //check for memory allocation
-        printf("memory allocation failed!!! \n");
+        printf("OS_A2@custom_shell:~$ Memory allocation failed!\n");
         exit(1);
     }
 
     if (fgets(buffer, MAX, stdin)==NULL){   //check for reading user input
-        printf("could not read user code!!\n");
+        printf("OS_A2@custom_shell:~$ Could not read input\n");
         exit(1);
     }
+    int length = strlen(buffer);
+
+    // for (int i=0; i<length; i++){
+    //     printf("%c", buffer[i]);
+    // }
 
     for (int i=0;i<length;i++){        //check for backslash and quotes
-        if (buffer[i]=='\"'|| buffer[i]=="'"|| buffer[i]=='""'){
-            printf("invalid input contains backslashes or quotes!! \n");
+        if (buffer[i]== '\\' || buffer[i]=='"'){
+            printf("OS_A2@custom_shell:~$ Invalid Input, contains backslashes or quotes!\n");
             exit (1);
         }
     }
 
     for (int i=0;i<length;i++){   //replacing new line character
         if (buffer[length-1]=="\n"){
-            buffer[length-1]="\O";
+            buffer[length-1]="\0";
         }
     }
  
@@ -63,6 +67,7 @@ void shell_loop() {
         char* command = read_user_input();
         status = launch(command);
         free(command); // Free the dynamically allocated input
+        //status = 1;
     } while (!status);
 }
 
